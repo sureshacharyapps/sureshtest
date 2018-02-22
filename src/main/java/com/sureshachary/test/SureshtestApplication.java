abc8631d52b5081a9465db18306e269900682e37
Suresh Achary <sureshachary@gmail.com>
2018-02-22 22:48:26 +0530

    Initial Commit

---
+++ src/main/java/com/sureshachary/test/SureshtestApplication.java
@@ -0,0 +1,12 @@
+package com.sureshachary.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SureshtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SureshtestApplication.class, args);
+	}
+}
